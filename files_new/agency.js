7001813cb436502fd7aa1bdf9c28ecb7a0fe2e07
msrocean <CompSec@583004949>
2026-07-14 02:26:37 -0600

    updated page

--- files_new/agency.js
+++ files_new/agency.js
@@ -5,14 +5,30 @@
  */
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
+function getNavbarOffset() {
+    var navbarHeight = $('.navbar-fixed-top').outerHeight() || 0;
+
+    return navbarHeight + 12;
+}
+
+function syncScrollOffset() {
+    document.documentElement.style.setProperty('--section-scroll-offset', getNavbarOffset() + 'px');
+}
+
 $(function() {
+    syncScrollOffset();
+
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
+        var targetTop = $($anchor.attr('href')).offset().top - getNavbarOffset();
+
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
+            scrollTop: Math.max(targetTop, 0)
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
+
+    $(window).on('resize', syncScrollOffset);
 });
 
 // Highlight the top nav as scrolling occurs
